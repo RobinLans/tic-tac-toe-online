537dc7739d896276fa83b87664cdde7547a229f7
Robin Lans <roblan95@icloud.com>
2022-01-29 10:42:23 +0100

    new font

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NameInput from "./components/NameInput";
 
 function App() {
     return (
-        <div className="bg-bg bg-cover font-nunito w-screen h-screen flex flex-col items-center">
+        <div className="bg-bg bg-cover font-gloria w-screen h-screen flex flex-col items-center">
             <img src={logo} alt="logo" className="h-52 mt-4 mb-32" />
             <NameInput />
         </div>
